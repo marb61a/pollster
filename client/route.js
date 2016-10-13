ab368d6a0864d835b3d3850fe14bc144b8493b97
martin byrne <marb61a@gmail.com>
2016-10-13 13:17:00 +0000

    added new routes

--- client/route.js
+++ client/route.js
@@ -6,6 +6,10 @@ import App from '../imports/ui/App.jsx';
 import Home from '../imports/ui/Home.jsx';
 import NewPoll from '../imports/ui/NewPoll.jsx';
 import Header from '../imports/ui/partials/header.jsx';
+import NewUser from '../imports/ui/partials/register.jsx';
+import Login from '../imports/ui/partials/login.jsx';
+import ChangePassword from '../imports/ui/partials/changePassword.jsx';
+import ResetPassword from '../imports/ui/partials/resetPassword.jsx';
 
 FlowRouter.route('/', {
     name : 'home',
@@ -36,3 +40,43 @@ FlowRouter.route('/newPoll', {
         });
     }
 });
+
+FlowRouter.route('/login', {
+    name : 'login',
+    action(params){
+        mount(MainLayout,{
+            header: (<Header />),
+            content: (<Login />)
+        });
+    }
+});
+
+FlowRouter.route('/register',{
+    name : 'register',
+    action(){
+        mount(MainLayout, {
+            header: (<Header />),
+            content: (<NewUser />)
+        });
+    }
+});
+
+FlowRouter.route('/change-password',{
+    name : 'change-password',
+    action(){
+        mount(MainLayout, {
+            header: (<Header />),
+            content: (<ChangePassword />)
+        });
+    }
+});
+
+FlowRouter.route('/reset-password', {
+    name : 'reset-password',
+    action(){
+        mount(MainLayout, {
+            header: (<Header />),
+            content: (<ResetPassword />)
+        });
+    }
+})
