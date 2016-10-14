e0ac813f1e5753acd9ccb3d3db35097b15c508b3
martin byrne <marb61a@gmail.com>
2016-10-14 18:52:59 +0000

    added footer and updated css

--- client/route.js
+++ client/route.js
@@ -6,6 +6,7 @@ import App from '../imports/ui/App.jsx';
 import Home from '../imports/ui/Home.jsx';
 import NewPoll from '../imports/ui/NewPoll.jsx';
 import Header from '../imports/ui/partials/header.jsx';
+import Footer from '../imports/ui/partials/footer.jsx';
 import NewUser from '../imports/ui/partials/register.jsx';
 import Login from '../imports/ui/partials/login.jsx';
 import ChangePassword from '../imports/ui/partials/changePassword.jsx';
