0f3ce747434feaf8712b3f904ad520f875512919
martin byrne <marb61a@gmail.com>
2016-10-16 18:43:32 +0000

    updated newPoll and added newPollOption file

--- imports/ui/NewPoll.jsx
+++ imports/ui/NewPoll.jsx
@@ -1,10 +1,41 @@
 import React, {Component, PropTypes} from 'react';
 
+import NewPollOption from './NewPollOptions.jsx'
+import OptionsData from '/imports/api/newPollOptions.js';
+
 export default class NewPoll extends Component{
+    getOptions(){
+        if(OptionsData.find().count() < 2){
+            this.addOption("", 0);
+            this.addOption("", 1);
+        }
+        
+        return OptionsData.find({}, {sort: {index: 1}}).fetch();
+    }
+    
+    addOption(option, index, e){
+        OptionsData.insert({
+            option : option,
+            index : index
+        });
+    }
+    
+    addNewPoll(e){
+        
+    }
+    
     render(){
+        const options = this.getOptions().map((option) => {
+            return <NewPollOption option={option} key={option.index}/>;
+        });
+        
         return(
-            <div className="billboard">
-                <h2>Make new Polls</h2>
+            <div>
+                <div>
+                    <h1 className="new-poll-title">Create New Poll</h1>
+                </div>
+                <div>
+                </div>
             </div>    
         );
     }
---
+++ imports/ui/NewPollOptions.jsx
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+import OptionsData from '/imports/api/newPollOptions.js';
+
+export default class NewPollOption extends Component {
+    render(){
+        return(
+            <div className="input-option">
+            
+            </div>  
+        );
+    }
+}
