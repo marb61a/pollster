8ad47932c548d9fb634c00a549afa59409670404
martin byrne <marb61a@gmail.com>
2016-10-19 18:00:59 +0000

    updated polls js

--- imports/api/polls.js
+++ imports/api/polls.js
@@ -2,6 +2,38 @@ import { Mongo } from 'meteor/mongo';
 
 const PollsData = new Mongo.Collection('polls');
 
+const pollOptionsSchema = new SimpleSchema({
+    index: {
+        type: Number
+    },
+    option: {
+        type: String
+    },
+    votes: {
+        type: Number,
+        defaultValue: 0
+    }
+});
+
+const pollsSchema = new SimpleSchema({
+    question : {
+        type : String
+    },
+    options : {
+        type : [pollOptionsSchema]
+    },
+    author : {
+        type: String,
+        autoValue: function() {
+            if (this.isInsert) {
+                return this.userId;
+            }  else {
+                this.unset();  // Prevent user from supplying their own value
+            }
+        }
+    },
+});
+
 if(Meteor.isServer){
     Meteor.methods({
         insertPoll(data){
