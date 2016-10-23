db7b697333d630a5d115d17d7ac33c53a92ad565
martin byrne <marb61a@gmail.com>
2016-10-23 10:52:13 +0000

    updated viewpoll with iframe

--- imports/ui/ViewPoll.jsx
+++ imports/ui/ViewPoll.jsx
@@ -16,6 +16,25 @@ export default class ViewPoll extends Component {
         });
     }
     
+    embedCode(){
+        const n = this.props.poll.options.length;
+        const height = (n + 1) * 40 + 50;
+        const routeName = "pollPost";
+        const protocol = window.location.protocol + "//";
+        const host = window.location.hostname;
+        let port = ":" + window.location.port;
+        if(window.location.port == 80)
+            port = "";
+        const params = {
+            id: this.props.poll._id
+        };
+        const path = protocol + host + port + FlowRouter.path(routeName, params);
+        const embed = '<iframe width="100%" height="'
+                    + height + '" src="' + path
+                    +'" frameborder="0" ></iframe>';
+        return embed;
+    }
+    
     render(){
         return(
             <div className="poll-view">
@@ -27,7 +46,17 @@ export default class ViewPoll extends Component {
                         {this.renderOptions()}
                     </div>
                 </div>
-            </div>    
+                <div className="url">
+                    <div className="instructions">
+                        <div className="title">
+                            <h4 className="">Copy and Paste this code into your page to embed this poll</h4>
+                        </div>
+                        <div className="panel-body iframe-code">
+                            {this.embedCode()}
+                        </div>
+                    </div>
+                </div>
+            </div>  
         );
     }
 }
